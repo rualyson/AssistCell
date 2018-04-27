7167dfa8726b45844a1f9a12f351ca4880397a92
Rualyson Cavalcante <rualyson.cavalcante@dce.ufpb.br>
2018-04-26 23:27:29 -0300

    Adição de categorias

--- app/src/main/java/com/online/assistencia/assistcell/TabCatalogo.java
+++ app/src/main/java/com/online/assistencia/assistcell/TabCatalogo.java
@@ -7,25 +7,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class TabCatalogo extends Fragment {
-
-
-    public TabCatalogo() {
-        // Required empty public constructor
-    }
-
+public class TabCatalogo extends Fragment{
+    ImageButton icon_tela;
+    ImageButton icon_fones;
+    ImageButton icon_capas;
+    ImageButton icon_pelicula;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalogo, container, false);
+        View view = inflater.inflate(R.layout.fragment_catalogo, container, false);
+        icon_tela = (ImageButton)view.findViewById(R.id.icon_tela);
 
+        icon_tela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), GrupoTelas.class));
+            }
+        });
+        icon_fones = (ImageButton)view.findViewById(R.id.icon_fones);
+        icon_fones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), GrupoFones.class));
+            }
+        });
+
+        icon_capas = (ImageButton)view.findViewById(R.id.icon_capas);
+        icon_fones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), GrupoCases.class));
+            }
+        });
+
+        icon_pelicula = (ImageButton)view.findViewById(R.id.icon_pelicula);
+        icon_pelicula.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), GrupoPeliculas.class));
+            }
+        });
+
+        return view;
     }
 
 }
