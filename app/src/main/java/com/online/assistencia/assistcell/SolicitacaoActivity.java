db6d9bf41444ea90d9448fda0104a6977dd3c13c
lipeacelino <felipe.acelino@dce.ufpb.br>
2018-04-29 23:52:10 -0300

    Adição de novos campos de texto, e adição de uma mensagem de erro visível ao usuário caso ele deixe de preencher algum campo obrigatório.

--- app/src/main/java/com/online/assistencia/assistcell/SolicitacaoActivity.java
+++ app/src/main/java/com/online/assistencia/assistcell/SolicitacaoActivity.java
@@ -7,9 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SolicitacaoActivity extends AppCompatActivity {
+    EditText editMarca;
+    EditText editModelo;
     EditText editProduto;
+    EditText editQuant;
     EditText editNome;
     EditText editEmail;
     EditText editTelefone;
@@ -20,21 +24,30 @@ public class SolicitacaoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitacao);
 
-        editProduto = (EditText) findViewById(R.id.editNome);
+        editMarca = (EditText) findViewById(R.id.editMarca);
+        editModelo = (EditText) findViewById(R.id.editModelo);
+        editProduto = (EditText) findViewById(R.id.editProduto);
+        editQuant = (EditText) findViewById(R.id.editQuant);
         editNome = (EditText) findViewById(R.id.editNome);
         editTelefone = (EditText) findViewById(R.id.editTelefone);
         editEmail = (EditText) findViewById(R.id.editEmail);
         botEnviar = (Button) findViewById(R.id.botEnviar);
 
-        /*
-        botEnviar.setOnClickListener (new View.OnClickListener()){
-            public void onClick(View view) {
-                if (editProduto.getText().toString().trim().equals("") || editNome.getText().toString().trim().equals("") || editTelefone.getText().toString().trim().equals("")) {
-                    editProduto.setError("Campo vazio");
-                    editNome.setError("Campo vazio");
-                    editTelefone.setError("Campo vazio");
+
+        botEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editProduto.getText().length() == 0 || (editNome.getText().length() == 0) || (editTelefone.getText().length() == 0)
+                        || editQuant.getText().length() == 0 || editMarca.getText().length() == 0 || editModelo.getText().length() == 0) {
+                    Toast.makeText(getApplication(), "Os campos 'Marca', 'Modelo', 'Descrição do produto', 'Quantidade', 'Nome' e 'Seu Telefone' são obrigatórios!",
+                            Toast.LENGTH_LONG).show();
+                    //não coloquei email como campo obrigatório porquê
+                    // o cliente que deseja um determinado produto pode não ter email, mas com certeza terá um telefone
+                } else {
+                    Toast.makeText(getApplication(),
+                            "Produto solicitado com Sucesso!", Toast.LENGTH_LONG).show();
                 }
             }
-        }*/
+        });
     }
 }
