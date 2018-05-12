0af0a69947e5998a185f3b5db7cd8a096ff2cd52
Rualyson Cavalcante <rualyson.cavalcante@dce.ufpb.br>
2018-05-12 00:51:51 -0300

    Implementação de tratamento de erros

---
+++ app/src/main/java/com/online/assistencia/assistcell/CadastroFuncionarios.java
@@ -0,0 +1,105 @@
+package com.online.assistencia.assistcell;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CadastroFuncionarios extends AppCompatActivity implements View.OnClickListener{
+    private EditText emailFunc;
+    private EditText nomeFunc;
+    private EditText cpfFunc;
+    private EditText enderecoFunc;
+    private EditText telefoneFunc;
+    private EditText senhaFunc;
+    private Button cadastrar;
+
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_funcionarios);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostra o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Faz funcionar o botão
+        getSupportActionBar().setTitle("Cadastro de funcionários");//Titulo para ser exibido na Action Bar
+
+        progressBar = new ProgressBar(this);
+
+        nomeFunc = (EditText) findViewById(R.id.editText);
+        cpfFunc = (EditText) findViewById(R.id.editText2);
+        enderecoFunc = (EditText) findViewById(R.id.editText4);
+        telefoneFunc = (EditText) findViewById(R.id.editText3);
+
+        emailFunc = (EditText) findViewById(R.id.editText7);
+        senhaFunc = (EditText) findViewById(R.id.editText5);
+        cadastrar = (Button) findViewById(R.id.buttonCadastrar);
+
+        cadastrar.setOnClickListener(this);
+        }
+    private void registrerUser(){
+        String email = emailFunc.getText().toString().trim();
+        String senha = senhaFunc.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Insira um E-mail válido!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(senha)){
+            Toast.makeText(this,"Insira uma senha!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        firebaseAuth.createUserWithEmailAndPassword(email,senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CadastroFuncionarios.this, "Registrado com sucesso!",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(CadastroFuncionarios.this, "Falha ao registrar",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão
+                startActivity(new Intent(this, EscCadastro.class));
+                finishAffinity();  //Matar activity
+                break;
+            default:break;
+        }
+        return true;
+    }
+    @Override
+    public void onClick(View view) {
+        if(view == cadastrar){
+                if (nomeFunc.getText().length() == 0 || cpfFunc.getText().length() == 0 || enderecoFunc.getText().length() == 0 || (telefoneFunc.getText().length() == 0)) {
+                    Toast.makeText(getApplication(), "Os campos Nome, CPF, Endereço e Telefone são obrigatórios, por favor preencher corretamente!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+            registrerUser();
+    }
+}
--- app/src/main/java/com/online/assistencia/assistcell/EscCadastro.java
+++ app/src/main/java/com/online/assistencia/assistcell/EscCadastro.java
@@ -1,13 +1,40 @@
 package com.online.assistencia.assistcell;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class EscCadastro extends AppCompatActivity {
+public class EscCadastro extends AppCompatActivity implements View.OnClickListener{
 
+    private CardView solicitCadastro;
+    private CardView cadastroFuncionario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esc_cadastro);
+
+        solicitCadastro = (CardView) findViewById(R.id.RequestCad);
+        solicitCadastro.setOnClickListener(this);
+        cadastroFuncionario = (CardView) findViewById(R.id.CadFunc);
+        cadastroFuncionario.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.RequestCad:
+                i = new Intent(this, SolicitLogin.class);
+                startActivity(i);
+                break;
+            case R.id.CadFunc:
+                i = new Intent(this, CadastroFuncionarios.class);
+                startActivity(i);
+                break;
+        }
     }
 }
