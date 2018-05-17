d642853460e29c9a385d530c4ecb544675115b27
Rualyson Cavalcante <rualyson.cavalcante@dce.ufpb.br>
2018-05-17 19:01:43 -0300

    Validação de dados

--- app/src/main/java/com/online/assistencia/assistcell/CadServices.java
+++ app/src/main/java/com/online/assistencia/assistcell/CadServices.java
@@ -4,8 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class CadServices extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
+public class CadServices extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nomeCli, Marca, Modelo,Imei, EstadoF, Relato, Contato, Email, Horario, txtOutros;
+    private RadioButton ac_chip;
+    private RadioButton ac_cardSD;
+    private RadioButton ac_Carregador;
+    private RadioButton ac_Outros;
+    private Button enviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +32,51 @@ public class CadServices extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Cadastro de serviços");
+
+        nomeCli = (EditText) findViewById(R.id.editNomeS);
+        Marca = (EditText) findViewById(R.id.editMarcaS);
+        Modelo = (EditText) findViewById(R.id.editModeloS);
+        Imei = (EditText) findViewById(R.id.editImei);
+        EstadoF = (EditText) findViewById(R.id.editEstadoFisicoS);
+        Relato = (EditText) findViewById(R.id.editProblemaS);
+        Contato = (EditText) findViewById(R.id.editPhoneS);
+        Email = (EditText) findViewById(R.id.editEmailCliS);
+        Horario = (EditText) findViewById(R.id.editHorario);
+        txtOutros = (EditText) findViewById(R.id.editOutrosText);
+        ac_chip = (RadioButton) findViewById(R.id.rbn_chip);
+        ac_cardSD = (RadioButton) findViewById(R.id.rbn_cartao);
+        ac_Carregador = (RadioButton) findViewById(R.id.rbn_carregador);
+        ac_Outros = (RadioButton) findViewById(R.id.rbn_outros);
+
+        enviar = (Button) findViewById(R.id.btnEnviarS);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nomeCli.getText().length() == 0 || (Marca.getText().length() == 0) || (Modelo.getText().length() == 0)
+                        || Horario.getText().length() == 0 || Imei.getText().length() == 0 || EstadoF.getText().length() == 0 || Relato.getText().length() == 0 || Contato.getText().length() == 0 || Email.getText().length() == 0) {
+                    Toast.makeText(getApplication(), "Todos os campos devem ser preenchidos!",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplication(), "Concluido com sucesso!",
+                            Toast.LENGTH_LONG).show();
+                    limparCampos();
+                }
+            }
+        });
+
+    }
+
+    private void limparCampos() {
+        nomeCli.setText("");
+        Marca.setText("");
+        Modelo.setText("");
+        EstadoF.setText("");
+        Imei.setText("");
+        Relato.setText("");
+        Horario.setText("");
+        Contato.setText("");
+        Email.setText("");
+        txtOutros.setText("");
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,5 +88,10 @@ public class CadServices extends AppCompatActivity {
             default:break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
