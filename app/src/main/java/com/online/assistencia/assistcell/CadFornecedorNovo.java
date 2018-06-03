d853cc5d867a487389dae40fbe0cac5f6d370d28
Rualyson Cavalcante <rualyson.cavalcante@dce.ufpb.br>
2018-06-03 15:39:33 -0300

    Back - Cadastro de fornecedores

--- app/src/main/java/com/online/assistencia/assistcell/CadFornecedorNovo.java
+++ app/src/main/java/com/online/assistencia/assistcell/CadFornecedorNovo.java
@@ -1,7 +1,11 @@
 package com.online.assistencia.assistcell;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,21 +18,7 @@ import static com.online.assistencia.assistcell.R.layout.activity_cad_fornecedor
 public class CadFornecedorNovo extends AppCompatActivity {
 
 
-    private EditText editCadEmp;
-    private EditText editCadRep;
-    private EditText editCadEnd;
-    private EditText editCadNum;
-    private EditText editCadBai;
-    private EditText editCadCid;
-    private EditText editCadCep;
-    private EditText editCadUf;
-    private EditText editCadPais;
-    private EditText editCadCom;
-    private EditText editCadRef;
-    private EditText editCadCnpj;
-    private EditText editCadEma;
-    private EditText editCadTel;
-
+    private EditText editCadEmp,editCadRep,editCadEnd,editCadNum,editCadBai,editCadCid,editCadCep,editCadUf,editCadPais,editCadCom,editCadRef,editCadCnpj,editCadEma,editCadTel;
     private Button botCad;
 
     @Override
@@ -36,6 +26,9 @@ public class CadFornecedorNovo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(activity_cad_fornecedor_novo);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Cadastro de Fornecedores");
 
         editCadEmp = (EditText) findViewById(R.id.editCadEmp);
         editCadRep = (EditText) findViewById(R.id.editCadRep);
@@ -71,11 +64,49 @@ public class CadFornecedorNovo extends AppCompatActivity {
 
 
                 else {
-                    Toast.makeText(getApplication(),
-                            "Fornecedor cadastrado com Sucesso!", Toast.LENGTH_LONG).show();
+
+                    AlertDialog show = new AlertDialog.Builder(CadFornecedorNovo.this)
+                            .setTitle("Sucesso")
+                            .setMessage("Fornecedor cadastrado com Sucesso!")
+                            .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    CadFornecedorNovo.this.finish();
+                                }
+                            })
+                            .show();
+                    limparCampos();
                 }
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, EscFornecedor.class));
+                finishAffinity();
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    private void limparCampos() {
+        editCadEmp.setText("");
+        editCadRep.setText("");
+        editCadEnd.setText("");
+        editCadNum.setText("");
+        editCadBai.setText("");
+        editCadCid.setText("");
+        editCadCep.setText("");
+        editCadUf.setText("");
+        editCadPais.setText("");
+        editCadCom.setText("");
+        editCadRef.setText("");
+        editCadCnpj.setText("");
+        editCadEma.setText("");
+        editCadTel.setText("");
+
     }
 
 }
